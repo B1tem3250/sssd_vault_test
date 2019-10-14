17e58d82213c117b5a4e5405ae18577f7b4be0c8
f4851773 <cameron.hall@fnb.co.za>
2019-10-14 14:35:41 +0200

    renaming files

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 #
 include_recipe 'sssd_ldap::default'
+
 chef_gem 'vault' do
   action :nothing
 end.run_action(:install)
@@ -13,7 +14,7 @@ require 'vault'
 
 read_vault 'Read secret at secret/data/my-app' do
   path 'secret/data/my-app'
-  address 'http://vaultserver:8200'
+  address 'http://127.0.0.1:8200'
   token 'your-token-here'
   role_name 'chef-role'
   notifies :create, 'template[/etc/sssd/sssd.conf]', :immediately
@@ -22,22 +23,17 @@ end
 edit_resource(:template, '/etc/sssd/sssd.conf') do
   source 'sssd.erb'
   cookbook 'sssd_vault_test'
+  ## If I add the below then I get a error no method found for "data"
+  #  variables lazy {
+  #    {
+  #      ldap_default_bind_dn: node.run_state['secret/data/my-app'].data[:data][:username],
+  #      ldap_default_authtok: node.run_state['secret/data/my-app'].data[:data][:password],
+  #    }
+  #  }
 end
-
-# template '/etc/sssd/sssd.conf' do
-#  source 'sssd.erb'
-#  sensitive true
-#  variables lazy {
-#    {
-#    ldap_default_bind_dn: node.run_state['secret/data/my-app'].data[:data][:username],
-#    ldap_default_authtok: node.run_state['secret/data/my-app'].data[:data][:password],
-#    }
-#  }
-# end
 
 template '/etc/sssd/sssd.conf' do
   source 'sssd.erb'
-  sensitive true
   variables lazy {
     {
     ldap_default_bind_dn: node.run_state['secret/data/my-app'].data[:data][:username],
